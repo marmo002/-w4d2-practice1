ef4c7022dae9d2faa62ef48b57eaaef36a466f7a
Martin <martin.st8n@gmail.com>
2019-01-29 20:02:07 -0500

    created view about_me and my_links to respond to /about-me, and favorites

--- 03-my_first_web_app/server.rb
+++ 03-my_first_web_app/server.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
 
+get '/' do
+  redirect to '/home'
+end
+
 get '/home' do
   erb :index
+end
+
+get '/gallery' do
+  redirect to '/portfolio'
 end
 
 get '/portfolio' do
@@ -9,5 +17,15 @@ get '/portfolio' do
 end
 
 get '/about-me' do
+
+  @skills       =   %w(git HTML CSS Ruby)
+  @interests    =   %w(films coffee soccer ruby js php)
+
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links    =   %w(marca.com imdb.com google.ca)
+
+  erb :my_links
 end
